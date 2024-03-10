b99d97bd332c091b9fbcbbd0a8266294010f2a9c
m-sarful <isarful823@gmail.com>
2024-03-10 09:27:36 +0600

    explore react more concepts

--- explore-more-concepts/src/App.jsx
+++ explore-more-concepts/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Timer from './Timer';
 import Team from './Team';
 import Users from './Users';
-
+import Friends from './Friends';
 function App() {
 
   function handleClick(){
@@ -22,9 +22,14 @@ function App() {
     <>
      
       <h3>Explore more concepts of React</h3>
+
+
+      <Friends></Friends>
       <Users></Users>
       <Team></Team>
        <Timer></Timer>
+
+
       {/* <button onClick={handleClick}>Click Me!</button>
       <button onClick={handleClick2}>Click Me2!</button>
       <button onClick={()=>{alert('button clicked from inline js')}}>Click Me3!</button> */}
---
+++ explore-more-concepts/src/Friend.jsx
@@ -0,0 +1,10 @@
+import './Timer.css'
+export default function Friend({friend}){
+    const {name, email} = friend;
+    return(
+        <div className='box'>
+            <h3>Name : {name}</h3>
+            <p>E-mail : {email}</p>
+        </div>
+    )
+}
